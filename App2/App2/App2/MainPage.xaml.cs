f64a61a30c53e0ea92e45b1008043e695a9528fa
FinestOldToby <svobost17@sps-prosek.cz>
2020-10-11 19:23:37 +0200

    1st version calculator

---
+++ App2/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnPrecist(object sender, EventArgs e)
+        {
+            double a = Double.Parse(cislo1.Text);
+            double b = Double.Parse(cislo2.Text);
+            vysledek.Text = (a + b).ToString();
+        }
+        private void BtnOdecist(object sender, EventArgs e)
+        {
+            double a = Double.Parse(cislo1.Text);
+            double b = Double.Parse(cislo2.Text);
+            vysledek.Text = (a - b).ToString();
+        }
+        private void BtnVynasobit(object sender, EventArgs e)
+        {
+            double a = Double.Parse(cislo1.Text);
+            double b = Double.Parse(cislo2.Text);
+            vysledek.Text = (a * b).ToString();
+        }
+        private void BtnDelit(object sender, EventArgs e)
+        {
+            double a = Double.Parse(cislo1.Text);
+            double b = Double.Parse(cislo2.Text);
+            vysledek.Text = (a / b).ToString();
+        }
+        private void BtnDelitAZiskatZbytek(object sender, EventArgs e)
+        {
+            double a = Double.Parse(cislo1.Text);
+            double b = Double.Parse(cislo2.Text);
+            vysledek.Text = (a % b).ToString();
+        }
+        private void BtnVycistitPole(object sender, EventArgs e)
+        {
+            cislo1.Text = String.Empty;
+            cislo2.Text = String.Empty;
+            vysledek.Text = String.Empty;
+        }
+    }
+}
